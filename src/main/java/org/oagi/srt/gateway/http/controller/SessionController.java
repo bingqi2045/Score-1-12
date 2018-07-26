59257fd5af301dc3ff4ebb9d84c867f6d55e68c1
Hakju Oh <hakju.oh@gmail.com>
2018-07-26 17:29:23 -0400

    Add '/username' call

--- src/main/java/org/oagi/srt/gateway/http/controller/SessionController.java
+++ src/main/java/org/oagi/srt/gateway/http/controller/SessionController.java
@@ -1,18 +1,29 @@
 package org.oagi.srt.gateway.http.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class SessionController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public void login() {
+    }
+
+    @RequestMapping(value = "/username", method = RequestMethod.GET)
+    public Map<String, String> username(@AuthenticationPrincipal User user) {
+        Map<String, String> resp = new HashMap();
+        resp.put("username", user.getUsername());
+        return resp;
     }
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
