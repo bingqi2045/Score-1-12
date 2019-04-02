7545a03685468d5e0f078c035a617792dec859f1
Hakju Oh <hakju.oh@gmail.com>
2019-04-02 14:53:26 -0400

    Update from c80dbea

--- src/main/java/org/oagi/srt/gateway/http/SrtHttpGatewayApplication.java
+++ src/main/java/org/oagi/srt/gateway/http/SrtHttpGatewayApplication.java
@@ -2,11 +2,12 @@ package org.oagi.srt.gateway.http;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = "org.oagi.srt")
 @RestController
-public class SrtHttpGatewayApplication {
+public class SrtHttpGatewayApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SrtHttpGatewayApplication.class, args);
