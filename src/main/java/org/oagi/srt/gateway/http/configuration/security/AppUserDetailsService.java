cb85c1ce412a3b0f436e35ee9a07be3418fb6304
Hakju Oh <evanescent85@gmail.com>
2019-04-14 23:29:29 -0400

    Replace CustomUserDetails with 'User'.

--- src/main/java/org/oagi/srt/gateway/http/configuration/security/AppUserDetailsService.java
+++ src/main/java/org/oagi/srt/gateway/http/configuration/security/AppUserDetailsService.java
@@ -4,10 +4,13 @@ import org.jooq.DSLContext;
 import org.oagi.srt.entity.jooq.Tables;
 import org.oagi.srt.entity.jooq.tables.records.AppUserRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class AppUserDetailsService implements UserDetailsService {
@@ -16,7 +19,7 @@ public class AppUserDetailsService implements UserDetailsService {
     private DSLContext dslContext;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
         AppUserRecord appUserRecord = dslContext.selectFrom(Tables.APP_USER)
                 .where(Tables.APP_USER.LOGIN_ID.eq(username))
                 .fetchOptional().orElse(null);
@@ -24,6 +27,10 @@ public class AppUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException(username);
         }
 
-        return new AppUserDetails(appUserRecord);
+        String password = appUserRecord.get(Tables.APP_USER.PASSWORD);
+        boolean oagisDeveloper = (appUserRecord.getOagisDeveloperIndicator() == 1) ? true : false;
+
+        return new User(username, password,
+                Arrays.asList(new SimpleGrantedAuthority((oagisDeveloper) ? "developer" : "end-user")));
     }
 }
