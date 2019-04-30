b70e192567fec745bf98a693596aa6067d57236e
Hakju Oh <hakju.oh@gmail.com>
2019-04-30 14:36:44 -0400

    Add Dockerfile and its relatives.

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if ! [[ -x "$(command -v docker)" ]]; then
+  echo "Error: docker is not installed. See https://docs.docker.com/install/ for details.'" >&2
+  exit 1
+fi
+
+echo "Packaging project..."
+./mvnw clean package -DskipTests=true
+
+echo "Preparing files..."
+rm -f docker/*.war
+cp target/*.war docker
+
+echo "Building docker image..."
+cd docker
+docker build -f Dockerfile -t oagi1docker/srt-http-gateway:1.1.2 .
+cd ..
+
+echo "Done."
