9eb58f1c5794d8793a53d2189cfba4246607d412
Hakju Oh <hakju.oh@gmail.com>
2019-10-06 23:34:26 -0400

    Add Unique Key on `biz_ctx_assignment` table.

--- src/main/resources/biz_ctx_assignment_migrate.sql
+++ src/main/resources/biz_ctx_assignment_migrate.sql
@@ -6,7 +6,8 @@ CREATE TABLE `biz_ctx_assignment` (
   `biz_ctx_id` bigint(20) unsigned NOT NULL,
   `top_level_abie_id` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`biz_ctx_assignment_id`),
-  KEY `from_biz_ctx_id` (`biz_ctx_id`),
+  UNIQUE KEY `biz_ctx_assignment_uk` (`biz_ctx_id`,`top_level_abie_id`),
+  KEY `biz_ctx_id` (`biz_ctx_id`),
   KEY `top_level_abie_id` (`top_level_abie_id`),
   CONSTRAINT `biz_ctx_rule_ibfk_1` FOREIGN KEY (`biz_ctx_id`) REFERENCES `biz_ctx` (`biz_ctx_id`),
   CONSTRAINT `biz_ctx_rule_ibfk_2` FOREIGN KEY (`top_level_abie_id`) REFERENCES `top_level_abie` (`top_level_abie_id`)
