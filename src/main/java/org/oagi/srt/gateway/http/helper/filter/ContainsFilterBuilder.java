55622e57298154757df9b312ff4c80ba6c2a5772
Hakju Oh <hakju.oh@gmail.com>
2020-03-11 12:28:43 -0400

    Update Issue #774

--- src/main/java/org/oagi/srt/gateway/http/helper/filter/ContainsFilterBuilder.java
+++ src/main/java/org/oagi/srt/gateway/http/helper/filter/ContainsFilterBuilder.java
@@ -51,7 +51,7 @@ public class ContainsFilterBuilder {
 
     public static List<String> split(String q, String sep) {
         return Arrays.asList(q.split(sep)).stream()
-                .map(e -> e.replaceAll("[^a-zA-Z0-9]", "").trim()).collect(Collectors.toList());
+                .collect(Collectors.toList());
     }
 
     public static Collection<Condition> contains(String s, TableField field) {
