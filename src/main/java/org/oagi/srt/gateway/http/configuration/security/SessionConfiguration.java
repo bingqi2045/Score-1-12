8365e40395c9c26e86760922db5059897d2d0dd8
Hakju Oh <hakju.oh@gmail.com>
2020-05-28 14:12:52 -0400

    Add SerializerException handler.

--- src/main/java/org/oagi/srt/gateway/http/configuration/security/SessionConfiguration.java
+++ src/main/java/org/oagi/srt/gateway/http/configuration/security/SessionConfiguration.java
@@ -1,16 +1,30 @@
 package org.oagi.srt.gateway.http.configuration.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
 import org.springframework.session.web.http.CookieHttpSessionIdResolver;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.session.web.http.HttpSessionIdResolver;
+import org.springframework.session.web.http.SessionRepositoryFilter;
 
 @Configuration
 @EnableRedisHttpSession
-public class SessionConfiguration {
+public class SessionConfiguration extends RedisHttpSessionConfiguration {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public SessionConfiguration() {
+        setConfigureRedisAction(ConfigureRedisAction.NO_OP);
+    }
 
     @Bean
     public HttpSessionIdResolver httpSessionIdResolver() {
@@ -25,5 +39,11 @@ public class SessionConfiguration {
     @Bean
     public HttpSessionEventPublisher httpSessionEventPublisher() {
         return new HttpSessionEventPublisher();
+    }
+
+    @Override
+    public <S extends Session> SessionRepositoryFilter<? extends Session> springSessionRepositoryFilter(
+            SessionRepository<S> sessionRepository) {
+        return super.springSessionRepositoryFilter(new SafeDeserializationRepository(sessionRepository, redisTemplate));
     }
 }
