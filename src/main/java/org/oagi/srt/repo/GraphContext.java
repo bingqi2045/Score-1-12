4fa2bc4bef77fe697bf2b697ae83069813275d1a
Kwanghoon.Lee <rekuku1@gmail.com>
2020-06-04 22:17:03 -0400

    Add cardinalityMin/Max of DT_SC to graph

--- src/main/java/org/oagi/srt/repo/GraphContext.java
+++ src/main/java/org/oagi/srt/repo/GraphContext.java
@@ -125,6 +125,8 @@ public class GraphContext {
     public class DtScManifest {
         private ULong dtScManifestId;
         private ULong ownerDtManifestId;
+        private int cardinalityMin;
+        private int cardinalityMax;
         private String propertyTerm;
         private String representationTerm;
         private String State;
@@ -261,13 +263,16 @@ public class GraphContext {
         dtScManifestMap =
                 dslContext.select(DT_SC_MANIFEST.DT_SC_MANIFEST_ID, DT_SC_MANIFEST.OWNER_DT_MANIFEST_ID,
                         DT_SC.PROPERTY_TERM, DT_SC.REPRESENTATION_TERM, DT.STATE,
-                        DT_SC_MANIFEST.RELEASE_ID, DT_SC_MANIFEST.PREV_DT_SC_MANIFEST_ID)
+                        DT_SC_MANIFEST.RELEASE_ID, DT_SC_MANIFEST.PREV_DT_SC_MANIFEST_ID,
+                        DT_SC.CARDINALITY_MIN, DT_SC.CARDINALITY_MAX)
                         .from(DT_SC_MANIFEST)
                         .join(DT_SC).on(DT_SC_MANIFEST.DT_SC_ID.eq(DT_SC.DT_SC_ID))
                         .join(DT).on(DT_SC.OWNER_DT_ID.eq(DT.DT_ID))
                         .fetch(record -> new DtScManifest(
                                 record.get(DT_SC_MANIFEST.DT_SC_MANIFEST_ID),
                                 record.get(DT_SC_MANIFEST.OWNER_DT_MANIFEST_ID),
+                                record.get(DT_SC.CARDINALITY_MIN),
+                                record.get(DT_SC.CARDINALITY_MAX),
                                 record.get(DT_SC.PROPERTY_TERM),
                                 record.get(DT_SC.REPRESENTATION_TERM),
                                 record.get(DT.STATE),
@@ -473,6 +478,8 @@ public class GraphContext {
         node.setPrevManifestId(dtScManifest.getPrevDtScManifestId());
         node.put("propertyTerm", dtScManifest.getPropertyTerm());
         node.put("representationTerm", dtScManifest.getRepresentationTerm());
+        node.put("cardinalityMin", dtScManifest.getCardinalityMin());
+        node.put("cardinalityMax", dtScManifest.getCardinalityMax());
         return node;
     }
 }
