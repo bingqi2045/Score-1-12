2facd774ac13f905931598a48472a6921bb988d8
Kwanghoon.Lee <rekuku1@gmail.com>
2020-10-04 12:06:09 -0400

    Remove relation b/w Extension component ASCC and Extension components while association deleting

--- score-http/src/main/java/org/oagi/score/gateway/http/api/cc_management/service/CcNodeService.java
+++ score-http/src/main/java/org/oagi/score/gateway/http/api/cc_management/service/CcNodeService.java
@@ -125,28 +125,7 @@ public class CcNodeService extends EventHandler {
         DeleteAsccRepositoryRequest request =
                 new DeleteAsccRepositoryRequest(user, asccManifestId);
 
-        AsccManifestRecord asccManifestRecord = asccReadRepository.getAsccManifestById(asccManifestId);
-        AsccpRecord asccpRecord
-                = asccpReadRepository.getAsccpByManifestId(asccManifestRecord.getToAsccpManifestId().toBigInteger());
-
         asccWriteRepository.deleteAscc(request);
-
-        if (asccpRecord.getType().equals(CcASCCPType.Extension.name())
-            && asccpRecord.getPropertyTerm().equals("Extension")) {
-            AsccpManifestRecord asccpManifestRecord
-                    = asccpReadRepository.getAsccpManifestById(asccManifestRecord.getToAsccpManifestId().toBigInteger());
-
-            BigInteger asccpManifestId = asccpManifestRecord.getAsccpManifestId().toBigInteger();
-            BigInteger accManifestId = asccpManifestRecord.getRoleOfAccManifestId().toBigInteger();
-
-            DeleteAsccpRepositoryRequest deleteAsccpRepositoryRequest
-                    = new DeleteAsccpRepositoryRequest(user, asccpManifestId);
-            asccpWriteRepository.removeAsccp(deleteAsccpRepositoryRequest);
-
-            DeleteAccRepositoryRequest deleteAccRepositoryRequest
-                    = new DeleteAccRepositoryRequest(user, accManifestId);
-            accWriteRepository.removeAcc(deleteAccRepositoryRequest);
-        }
 
         fireEvent(new DeletedAsccEvent());
     }
